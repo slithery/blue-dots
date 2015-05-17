ad52df5fb4025cf679a3cc4a0d0cbdd4b68f5928
Slithery <rob@slithery.uk>
2015-05-17 09:22:51 +0100

    herbstluftwm - add dmenu2 power menu

---
+++ .bin/dmenu2_power_menu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# dmenu2 power menu
+#
+
+OPT=$(echo -e "lock\nsuspend\npoweroff\nreboot" | dmenu -f -h 28 -fn "Ubuntu Mono-12" -x 435 -y 370 -w 410 -nb black -nf white -sb white -sf black )
+
+case $OPT in
+    lock) slock ;;
+	suspend) sudo systemctl suspend ;;
+	poweroff) sudo systemctl poweroff ;;
+	reboot) sudo systemctl reboot ;;
+	*) ;;
+esac
+
